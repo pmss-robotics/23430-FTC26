7d34cef39280183a8c59790bf4a358b5ac79e52d
gjehman <gjehman@gmail.com>
2025-11-16 18:45:04 -0800

    Version 2025-10-23 --> no errors for Auto file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystemNew.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class IntakeSubsystemNew {
+    Telemetry telemetry;
+    DcMotorEx intakeMotor;
+
+    public IntakeSubsystemNew(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        intakeMotor = hardwareMap.get(DcMotorEx.class, "intakeMotor");
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void setPower(double power) {
+        double clippedPower = Range.clip(power, -1.0, 1.0);
+        intakeMotor.setPower(clippedPower);
+    }
+
+    public void periodic() {
+        telemetry.addData("Outtake Motor Power", intakeMotor.getPower());
+    }
+}
